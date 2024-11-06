c2b5497b733f279c39119473f77ab0c8dc89de7c
IgnacioPena13 <ignacio.penasepulveda@outlook.com>
2024-11-06 12:19:38 +0100

    Klockan och datum klart

--- src/components/clock.jsx
+++ src/components/clock.jsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Clock = () => {
   const date = new Date();
-  const hour = date.getHours();
-  const min = date.getMinutes();
-  const sec = date.getSeconds();
+  const today = date.toLocaleDateString();
+  const [actualTime, setActualTime] = useState("");
+  useEffect(() => {
+    setTimeout(() => {
+      setActualTime(date.toLocaleTimeString());
+    }, 1000);
+  });
 
   return (
     <div>
-      <h1>
-        {hour}:{min}:{sec}
-      </h1>
+      <h1>{actualTime}</h1>
+      <br />
+      <h2>{today}</h2>
     </div>
   );
 };
---
+++ src/components/elevlista.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Elevlista = () => {
+  return (
+    <ul>
+      <li>Yamen Alesa</li>
+      <li>Ignacio Pena</li>
+      <li>Ömer Faruk Peker</li>
+    </ul>
+  );
+};
+
+export default Elevlista;
