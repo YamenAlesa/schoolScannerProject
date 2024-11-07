094f63abeeb274f636937c2952490cca52eb6674
Yamen <yamenx2005@gmail.com>
2024-11-07 13:32:38 +0100

    Update package.json with new dependencies

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import "./App.css";
 import Layout from "./components/layout";
 import Clock from "./components/clock";
 function App() {
+  const apikey = process.env.REACT_APP_AIRTABLE_API_KEY;
+  console.log(apikey);
+
   return (
     <Layout>
       <h1>Välkomna! This is the food app for NTI - for the pull request</h1>
