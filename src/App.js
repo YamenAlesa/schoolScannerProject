80496703c5e6bd0cd3baf145f49a05a2038f804e
Yamen <yamenx2005@gmail.com>
2024-11-07 10:13:02 +0100

    scan done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-
+import Layout from './components/layout';
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Layout> 
+
+    </Layout>
   );
 }
 
