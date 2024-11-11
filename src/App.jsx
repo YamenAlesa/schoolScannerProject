fbb445f8da1ab9162a3c6ca0b2c4b8a3ff615070
Yamen <yamenx2005@gmail.com>
2024-11-11 14:06:26 +0100

    Refactor App component to remove Scanner and handleBarcodeScan

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,16 @@
 import React, { useState } from "react";
-import Scanner from "./components/scanning";
 import AirtableComponent from "./components/airtableAPI";
 import Clock from "./components/clock";
 import Daymenu from "./components/daymenu";
 
 const App = () => {
-  const [scannedBarcode, setScannedBarcode] = useState("");
-
-  const handleBarcodeScan = (barcode) => {
-    setScannedBarcode(barcode);
-  };
-
   return (
     <div>
       <h1>Välkomna! This is the food app for NTI</h1>
       <hr />
       <Daymenu />
       <Clock />
-      <Scanner onScan={handleBarcodeScan} />
-      <AirtableComponent barcode={scannedBarcode} />
+      <AirtableComponent />
     </div>
   );
 };
