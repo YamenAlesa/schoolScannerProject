283f4aa7f2f873b78beab8223a9f76899b3a0c0e
Yamen <yamenx2005@gmail.com>
2024-11-22 16:10:00 +0100

    feat: add bouncing animation to header icon and improve styling

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,15 +1,31 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { PiForkKnifeBold } from "react-icons/pi";
-import Banner from "./banner";
 import Weatherapi from "./weatherapi";
+import Banner from "./banner";
 
 const Header = () => {
+  const [isBouncing, setIsBouncing] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIsBouncing(true);
+      setTimeout(() => {
+        setIsBouncing(false);
+      }, 3000); // Bounce duration
+    }, 300000); // cooldown or delay
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <header className="flex justify-around gap-3 p-4">
       <Weatherapi />
       <div className="flex flex-col items-center justify-center px-4 mr-96 h-[20VH] w-full mx-auto">
-        <h3 className="text-center flex text-5xl h-fit text-white w-fit p-2">
-          Välkommen! {<PiForkKnifeBold />}
+        <h3 className=" text-center flex text-5xl h-fit text-white w-fit p-2">
+          Välkommen!
+          <span className={isBouncing ? "bounce" : ""}>
+            <PiForkKnifeBold />
+          </span>
         </h3>
         <Banner text="Öppettider: 11:00 - 13:00" />
       </div>
