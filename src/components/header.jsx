7b7751a96dcb556d9b3b7fd50f20e70bd47ce9c4
Yamen <yamenx2005@gmail.com>
2024-11-22 16:13:38 +0100

    fix: adjust padding in header component for better alignment

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -20,13 +20,12 @@ const Header = () => {
   return (
     <header className=" flex gap-4 p-4">
       <Weatherapi />
-      <div className="flex flex-col items-center justify-center px-4 mr-96 h-[20VH] w-full mx-auto">
+      <div className="flex flex-col items-center justify-center pl-16 mr-96 h-[20VH] w-full mx-auto">
         <h3 className=" text-center flex text-5xl h-fit text-white w-fit p-2">
           Välkommen!
           <span className={isBouncing ? "bounce" : ""}>
             <PiForkKnifeBold />
           </span>
-
         </h3>
         <Banner text="Öppettider: 11:00 - 13:00" />
       </div>
