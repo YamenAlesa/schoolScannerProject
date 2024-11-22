ccb014f25f8be198a65f29130334b3b835b68b6b
IgnacioPena13 <ignacio.penasepulveda@outlook.com>
2024-11-22 15:53:04 +0100

    changes to header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,10 +5,10 @@ import Weatherapi from "./weatherapi";
 
 const Header = () => {
   return (
-    <header className="flex justify-around gap-3 p-4">
+    <header className=" flex gap-4 p-4">
       <Weatherapi />
-      <div className="flex flex-col items-center justify-center px-4 mr-96 h-[20VH] w-full mx-auto">
-        <h3 className="text-center flex text-5xl h-fit text-white w-fit p-2">
+      <div className="flex flex-col items-center justify-center pl-16 mr-96 h-[20VH] w-full mx-auto">
+        <h3 className="flex text-5xl h-fit text-white w-fit p-2">
           Välkommen! {<PiForkKnifeBold />}
         </h3>
         <Banner text="Öppettider: 11:00 - 13:00" />
